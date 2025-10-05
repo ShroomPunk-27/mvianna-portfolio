c9c6a69a22b520b5051a26756e4c611d9b6673ee
M Vianna <morevu2002@gmail.com>
2025-10-05 12:28:47 -0300

    Delete contact/menudesplegable.php

--- contact/menudesplegable.php
+++
@@ -1,8 +0,0 @@
-<?php
-$myconn = mysql_connection('localhost', 'root', '', 'commissions');
-$mysql->select_db('commissions', $myconn) or die("No se pudo conectar a la base de datos");ç
-    $query = $mysql->query("SELECT * FROM comms_types");
-    while ($valores = mysqli_fetch_array($query)) {
-        echo '<option value="'.$valores['ID_commst'].'">'.$valores['type'].'</option>';
-    }
-?>
